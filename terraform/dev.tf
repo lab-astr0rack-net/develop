519287f86fdc9e3ff1fcaddd2cfeac55ef277bf8
Nathan Higley <contact@nathanhigley.com>
2025-09-01 11:53:43 -0500

    fix: bump terraform stuff

--- terraform/dev.tf
+++ terraform/dev.tf
@@ -1,17 +1,21 @@
 resource "proxmox_vm_qemu" "dev-vm" {
-  name    = var.vm_name
-  desc    = "Ephemeral Dev VM"
-  qemu_os = "l26"
-  bios    = "ovmf"
-  pool    = "lab"
+  name        = var.vm_name
+  description = "Ephemeral Dev VM"
+  qemu_os     = "l26"
+  bios        = "ovmf"
+  pool        = "lab"
   # Need to specify ide0 so the VM can see cloudinit CD
   boot        = "order=virtio0;ide0"
   target_node = var.pm_node
   onboot      = true
   startup     = "order=2"
+  skip_ipv6   = true
 
-  sockets    = 1
-  cores      = 2
+  cpu {
+    sockets = 1
+    cores   = 2
+  }
+
   memory     = 4096
   agent      = 1
   clone      = "ubuntu-docker"
@@ -22,14 +26,25 @@ resource "proxmox_vm_qemu" "dev-vm" {
   ciuser    = var.vm_user
   ipconfig0 = "ip=dhcp,ip6=auto"
 
-  network {
-    bridge = "vmbr0"
-    model  = "e1000"
+  disks {
+    virtio {
+      virtio0 {
+        ignore = true
+      }
+    }
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local"
+        }
+      }
+    }
   }
-  lifecycle {
-    ignore_changes = [
-      disk,
-    ]
+
+  network {
+    id     = 0
+    bridge = "vmbr20"
+    model  = "virtio"
   }
 
   os_type = "cloud-init"
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "2.9.14"
+      version = "3.0.2-rc04"
     }
     dns = {
       source  = "hashicorp/dns"
-      version = "3.3.2"
+      version = "3.4.3"
     }
   }
 }
